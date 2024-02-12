244c2185e5bc8d5c360534c3c9c17167f6b3e925
AndreasSteffensen <55832629+AndreasSteffensen@users.noreply.github.com>
2024-02-12 22:25:27 +0100

    making it a blazor app instead

---
+++ mealprep/Recipe.cs
@@ -0,0 +1,29 @@
+﻿namespace MealPrep
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+
+        public List<Ingredient> Ingredients { get; set;}
+
+        public Recipe(string name)
+        {
+            Name = name;
+        }
+
+        //TODO: add function that handles saving a recipe to a text file and with ingredients and all.
+
+    }
+
+    public class Ingredient
+    { 
+        public string Name { get; set; }
+        public float Quantity { get; set; }
+
+        public Ingredient(string name, float quantity) 
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+    }
+}
